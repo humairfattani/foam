982e0344e1f37364dc5556b293819cb6a8b6046a
humairfattani <humairfattani950@gmail.com>
2021-12-22 07:09:12 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+function Getvalue(){
+    var a = document.getElementById("rollno");
+    var b = document.getElementById("rg");
+    var c = document.getElementById("name")
+    var d = document.getElementById("faname")
+    var e = document.getElementById("date")
+    var f = document.getElementById("nat")
+    var g = document.getElementById("mf")
+    var h = document.getElementById("re")
+    var i = document.getElementById("address")
+    var j = document.getElementById("cnic")
+    var k = document.getElementById("contact")
+    var l = document.getElementById("MARK")
+    var m = document.getElementById("post")
+    var n = document.getElementById("ur")
+    var o = document.getElementById("ea")
+    var p = document.getElementById("mo")
+    var q = document.getElementById("his")
+    var r = document.getElementById("eng")
+    var s = document.getElementById("engl")
+
+
+    var ab = a.value;
+    var ac = b.value;
+    var ad = c.value;
+    var ae = d.value;
+    var af = e.value;
+    var ag = f.value;
+    var ah = g.value;
+    var ai = h.value;
+    var aj = i.value;
+    var ak = j.value;
+    var al = k.value;
+    var am = l.value;
+    var an = m.value;
+    var ao = n.value;
+    var ap = o.value;
+    var aq = p.value;
+    var ar = q.value;
+    var as = r.value;
+    var ar = s.value;
+    
+
+
+    document.write("<pre>")
+    document.writeln("Roll no :",ab,);
+    document.writeln("Registration No :",ac)
+    document.writeln("Name :",ad)
+    document.writeln("FATHER Name :",ae)
+    document.writeln("DATE OF BIRTH :",af)
+    document.writeln("NATIONALTY :",ag)
+    document.writeln("MALE/FEMALE :",ah)
+    document.writeln("RELIGION :",ai)
+    document.writeln("RESIDENTIAL ADDRESS :",aj)
+    document.writeln("C.N.I.C. No. :",ak)
+    document.writeln("CONTACT #: ",al)
+    document.writeln("MARK OF IDENTIFICATION: : ",am)
+    document.writeln("POST CODE: ",an)
+    document.writeln("COMPLSary SUBJECT: URDU NORMAl ",ao)
+    document.writeln("COMPLSary SUBJECT: URDU EASY ",ap)
+    document.writeln("COMPLSary SUBJECT: URDU NON MOTHER TONGUE ",aq)
+    document.writeln("COMPLSary SUBJECT: HISTORY & CULTURE OF PAKISTAN ",ar)
+    document.writeln("COMPLSary SUBJECT: ENGLISH NORMAL ",as)
+    document.writeln("COMPLSary SUBJECT: ENGLISH ADVANCED ",at)
+    document.write("</pre>")
+}
